5c5a974d41e91224e86e9334d538f972b321e518
akash chowrasia <akash@plaxonic.com>
2021-04-25 05:51:18 +0530

    :lock: security work

--- .env.sh
+++ .env.sh
@@ -1,5 +1,5 @@
 export APP_PORT=8080
-export REDIS_URL=redis://localhost:6379
+export REDIS_URL="redis-17424.c262.us-east-1-3.ec2.cloud.redislabs.com:17424"
 export URL_PREFIX="/api"
 export CORS_REGEX="localhost";
 export ELASTIC_APM_ACTIVE=false
